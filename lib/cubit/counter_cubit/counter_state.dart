265a2badfca869c37cb43dcd616ddd342cbb3d4d
AndroSaied <androsid15@yahoo.com>
2024-07-07 00:18:07 +0300

    create splash view part 1

--- lib/cubit/counter_cubit/counter_state.dart
+++
@@ -1,5 +0,0 @@
-class CounterState {}
-
-class CounterInitialState extends CounterState {}
-class CounterAIncrementState extends CounterState {}
-class CounterBIncrementState extends CounterState {}
